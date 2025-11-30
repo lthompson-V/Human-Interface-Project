50964a08d637acb0f1ddab23fbf1c9a0343a5e97
clickyquack <159969539+clickyquack@users.noreply.github.com>
2025-11-30 18:46:58 -0500

    fixed hover and accessibility button bugs

---
+++ ResourcesPage/article.js
@@ -0,0 +1,29 @@
+let moreTab = document.querySelector('.more-tab');
+let moreContent = document.querySelector('.more-contents');
+
+moreTab.addEventListener('mouseenter', () => {
+  moreContent.classList.add('show');
+});
+
+moreTab.addEventListener('mouseleave', () => {
+  setTimeout(() => {
+    if (!moreContent.matches(':hover')) {
+      moreContent.classList.remove('show');
+    }
+  }, 300);
+});
+
+moreContent.addEventListener('mouseleave', () => {
+  moreContent.classList.remove('show');
+});
+
+
+// accessibility popup
+
+
+let accessContainer = document.querySelector('.access-container');
+let accessBtn = document.querySelector('.fa-universal-access');
+
+accessBtn.addEventListener('click', () => {
+  accessContainer.classList.toggle('open');
+});
