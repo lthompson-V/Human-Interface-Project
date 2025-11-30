40f8dfd3e533f2f8c03e0b76eb01ceb9c05a3a13
Khalia Dogan <kdogan@kent.edu>
2025-11-29 23:18:07 -0500

    Implemented practice.js
    Added event listener for start practice button to validate selections and store them in local storage before redirecting to the test page.

--- PracticePage/practice.js
+++ PracticePage/practice.js
@@ -17,7 +17,31 @@ moreContent.addEventListener('mouseleave', () => {
   moreContent.classList.remove('show');
 });
 
+document.getElementById('start-practice-btn').addEventListener("click", () => {
+  let questiontype = [...document.querySelectorAll('input[name="question-type"]:checked')]
+  let subject = document.querySelector('input[name="subject"]:checked');
+  let numberOfQuestions = document.querySelector('input[name="number-of-questions"]:checked');
+  if (questiontype.length === 0 || !subject || !numberOfQuestions) {
+    alert("Please make sure to select for all required fields!");
+    return;
+  }
 
+  localStorage.setItem('selectedQuestionType', JSON.stringify(questiontype.map(q => q.value)));
+  localStorage.setItem('selectedSubject', subject.value);
+  localStorage.setItem('selectedNumberOfQuestions', numberOfQuestions.value);
+  //check if user wants custom number of questions and change variable in storage if thats the case
+  if (numberOfQuestions.value === "custom") {
+    const customValue = document.getElementById("custom-number-input").value;
+    if (!customValue || parseInt(customValue) < 1) {
+        alert("Please enter a valid custom number of questions (minimum 1).");
+        return;
+    }
+    localStorage.setItem("custom-number", customValue);
+  }
+
+
+  window.location.href = "TestPage/test.html"; //send user to test page
+});
 // accessibility popup
 
 
